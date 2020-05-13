cfa905c04445975ff1c1b0f3e3a0b2577690fd3f
Ewan Harris <ewanharris93@gmail.com>
2020-05-13 13:41:21 +0100

    feat: add android handling

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,11 +4,6 @@ if (OS_IOS) {
 	Ti.App.iOS.addEventListener('handleurl', (e) => {
 		/** 
 		 * When we launch on iOS we get an object like below, `launchOptions` is the URL used to launch the application.
-		 * This is just like a web URL and you can pass data along use query parameters, then the app can use those parameters to perform the required action
-		 * 
-		 * For example:
-		 * 	tisample://alert?somedata=foobar
-		 * 	tisample://updateLabel?somedata=123456
 		 * 
 		 * {
 		 *   "launchOptions":{
@@ -19,23 +14,24 @@ if (OS_IOS) {
 		 *   "source":{},
 		 *   "cancelBubble":false
 		 * }
-		*/
-		console.log(`Handled URL, data was ${JSON.stringify(e)}`);
-		const [ ignore, action, rawQueryParams ] = e.launchOptions.url.match(/tisample:\/\/(\w+)\??(.+)/);
-		const data = parseQueryParams(rawQueryParams);
-
-		switch (action) {
-			case 'alert':
-				alert(data);
-				break;
-			case 'updateLabel':
-				$.displayLabel.text = data;
-				break
-			default:
-				console.warn(`Unknown action ${action}`);
-				console.log(`data: ${JSON.stringify(data)}`);
-				break;
-		}
+		 */
+		console.log(`iOS handled event ${JSON.stringify(e)}`);
+		handleURL(e);
+	});
+} else if (OS_ANDROID) {
+	const deeply = require('ti.deeply');
+	deeply.setCallback((e) => {
+		/**
+		 * When we launch on Android we get an object like below, here we'll just normalise it to match iOS
+		 * {
+		 *   "data":"tisample://alert?name=Titanium&whatIsIt=awesome",
+		 *   "action":"android.intent.action.VIEW",
+		 *   "extras":null
+		 * }
+		 */
+		console.log(`Android handled event ${JSON.stringify(e)}`);
+		const eventData = { launchOptions: { url: e.data } };
+		handleURL(eventData);
 	});
 }
 
@@ -50,4 +46,32 @@ function parseQueryParams(url) {
 	}
 
 	return queryParams;
+}
+
+function handleURL (eventData) {
+	/**
+	 * The URL in an event is just like a web URL,
+	 * you can pass data along using query parameters and
+	 * then the app can then extract the action and data from the URL like below
+	 *
+	 * For example:
+	 * 	tisample://alert?somedata=foobar
+	 * 	tisample://updateLabel?somedata=123456
+	 */
+	console.log(`Handled URL, data was ${JSON.stringify(eventData)}`);
+	const [ ignore, action, rawQueryParams ] = eventData.launchOptions.url.match(/tisample:\/\/(\w+)\??(.+)/);
+	const data = parseQueryParams(rawQueryParams);
+
+	switch (action) {
+		case 'alert':
+			alert(data);
+			break;
+		case 'updateLabel':
+			$.displayLabel.text = data;
+			break
+		default:
+			console.warn(`Unknown action ${action}`);
+			console.log(`data: ${JSON.stringify(data)}`);
+			break;
+	}
 }
